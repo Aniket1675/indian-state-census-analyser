09ad540cbde4f90cae20c643a7c391f0e6e97991
Aniket1675 <Aniket@05>
2022-04-14 18:28:29 +0530

    ADDED: Code for Testing the Type Incorrect Custom Exception

--- src/main/java/com/bridgelabz/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/CensusAnalyserException.java
@@ -1,19 +1,22 @@
 package com.bridgelabz;
 
 
+/**
+ * @description create Class for Defining the Census Analyser Exception
+ *
+ */
 public class CensusAnalyserException extends Exception {
-
+    //Created Enum Exception Type
     enum ExceptionType {
         CENSUS_FILE_PROBLEM,UNABLE_TO_PARSE,DELIMITER_INCORRECT_PROBLEM
     }
-
     ExceptionType type;
-
+    //Created Custom Exception
     public CensusAnalyserException(String message, ExceptionType type) {
         super(message);
         this.type = type;
     }
-
+    //Created custom exception with Parameter
     public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
         super(message, cause);
         this.type = type;
